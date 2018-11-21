80f940ba5b0b40a4376e420fb61832686956f9b8
Cristiano1980 <41929507+Cristiano1980@users.noreply.github.com>
2018-11-21 00:40:56 -0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,2 +1,11 @@
 totalEmission <- aggregate(Emissions ~ year, NEIdata, sum)
 totalEmission
+
+
+barplot(
+  (totalEmission$Emissions)/10^6,
+  names.arg=totalEmission$year,
+  xlab="Year",
+  ylab="PM2.5 Emissions (10^6 Tons)",
+  main="Total PM2.5 Emissions From All US Sources"
+)
